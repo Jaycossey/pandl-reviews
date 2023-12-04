aaadcc98c4c00e1c1e07588e03c4fc2d23505851
jaycossey <jaycossey@live.com>
2023-12-04 11:38:02 +0000

    added form and section for contact, added section for staff details, awaiting data to complete

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,12 @@
 // GLOBALS ----------------------------------------------------------------
 const reviewTextContainer = document.getElementById('reviewText');
 const accordionElement = document.getElementById('reviewAccordion');
+const bandTitle = document.getElementById('bandNameDisplay');
+const albumArtContainer= document.getElementById('albumArtContainer');
 
 // Band name storage - handles searches and fetch
 let bandName = "lune";
+let albumArt = document.createElement('img');
 let currentReview;
 
 // FUNCTIONS TO CREATE NEW ACCORDIAN ELEMENT ------------------------------
@@ -27,12 +30,19 @@ const fetchReviews = async () => {
         })
 }
 
-fetchReviews();
+// fetchReviews();
 
 accordionElement.addEventListener('click', function(event) {
+    albumArtContainer.appendChild(albumArt);
+
     // console.log(event.target);
     if (event.target instanceof HTMLButtonElement) {
         bandName = event.target.innerText;
+        bandTitle.innerText = bandName;
+        
+        albumArt.id = "albumArt";
+        albumArt.alt = bandName + " album art";
+        albumArt.src = './assets/images/' + bandName + '.PNG';
         fetchReviews();
         console.log(bandName);
         console.log(event.target);
