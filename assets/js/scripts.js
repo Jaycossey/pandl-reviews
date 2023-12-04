83df3ff157766b5bc8267d500a06031ed8d03b17
jaycossey <jaycossey@live.com>
2023-12-04 12:40:18 +0000

    updated readme, updated comments on JS to indicate bug reports

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,20 +4,21 @@ const accordionElement = document.getElementById('reviewAccordion');
 const bandTitle = document.getElementById('bandNameDisplay');
 const albumArtContainer= document.getElementById('albumArtContainer');
 
-// Band name storage - handles searches and fetch
+// Band data storage - handles searches and fetch
 let bandName = "lune";
-let albumArt = document.createElement('img');
 let currentReview;
+// create element to handle album art
+let albumArt = document.createElement('img');
 
 // FUNCTIONS TO CREATE NEW ACCORDIAN ELEMENT ------------------------------
 
 // THIS IS THE FIX!!! needs to be run in the live server to show the log properly (extension wasnt working, or I had the wrong one)
-// credit for fix => @juhuyoon - github
+// credit for syntax fix => @juhuyoon - github
+
 // Arrow function to handle async fetch
 const fetchReviews = async () => {
     // custom path for each file
     let path = `./assets/text-files/${bandName}.txt`;
-    // console.log(path);
     // fetch data 
     fetch(path, {mode: 'no-cors'})
         .then((response) => {
@@ -30,21 +31,34 @@ const fetchReviews = async () => {
         })
 }
 
-// fetchReviews();
-
+// add event listener for accordion element -- BUG REPORT: See below, index: "Accordion Bug"
 accordionElement.addEventListener('click', function(event) {
+    // append album art element
     albumArtContainer.appendChild(albumArt);
 
     // console.log(event.target);
+    // if element is of type Button
     if (event.target instanceof HTMLButtonElement) {
+        // update bandName text
         bandName = event.target.innerText;
         bandTitle.innerText = bandName;
-        
+        // update album art element
         albumArt.id = "albumArt";
         albumArt.alt = bandName + " album art";
         albumArt.src = './assets/images/' + bandName + '.PNG';
+        // fetch text data of reviews
         fetchReviews();
-        console.log(bandName);
-        console.log(event.target);
+        // console.log(bandName);
+        // console.log(event.target);
     }
 })
+
+/**
+ * BUG REPORTS:
+ * 
+ * - Accordion Bug:
+ *      At present, the accordion headers are functional as buttons but not as divs, so when event
+ *      listener is active, it changes the content to be the element clicked, need to target the child 
+ *      buttons with the listener not the parent elements. Could target with class or query selector rather
+ *      than with the entire accordion. 
+ */
